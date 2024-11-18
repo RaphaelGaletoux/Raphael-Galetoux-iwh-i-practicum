3a9a509cc897b236e2476715aaf064912c518bc0
Raphael Galetoux <raphael.galetoux@serli.com>
2024-11-18 15:17:42 +0100

    First route added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,11 +10,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const Events = 'https://api.hubapi.com/crm/v3/objects/2-37049875?properties=Name&properties=Description&properties=Date';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const response = await axios.get(Events, { headers });
+        const data = response.data.results;
+        res.render("home", {data});    
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
